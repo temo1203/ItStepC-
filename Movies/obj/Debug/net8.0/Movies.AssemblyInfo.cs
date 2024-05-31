d7fd0456ece442d277acfb86820aece48b9f9d54
temo1203 <temolortkipanidze2007@gmail.com>
2024-05-31 15:00:25 +0400

    changes

--- Movies/obj/Debug/net8.0/Movies.AssemblyInfo.cs
+++ Movies/obj/Debug/net8.0/Movies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a9c2411be2605e2380bfd8171dde8c822d46a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c369a931c11f5d87fcd97df34793e7e69fdb0f7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
